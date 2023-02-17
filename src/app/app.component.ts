8770388bfe60f77fb77c1c19e10705bf3b36d1a7
Luis Angel Sandoval Medina <sandovalguichoo@gmail.com>
2023-02-17 10:14:27 -0600

    v1.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,8 +14,7 @@ export class AppComponent implements OnInit {
   //-- Variables globales
 
   frase: EntityFrase[]=[new EntityFrase('Cargando...','Cargando...')];
-
-
+  mensajePersonalizado: String='';
 
   //-- Constructor
   constructor (
@@ -27,15 +26,27 @@ export class AppComponent implements OnInit {
   //-- Obtener frase aleatoria
   public obtenerFraseAleatoria():void{
     this.frase=[new EntityFrase('Cargando...','Cargando...')];
+
     this.apiBreakingService.buscarFraseRandom().subscribe(
       HttpResponse => {
         this.frase=HttpResponse;
+        this.mensajePersonalizado='';
       },
-      HttpErrorResponse=>{
-        console.error(HttpErrorResponse);
+
+      HttpErrorResponse=>{ 
+        switch (HttpErrorResponse.status) {
+          case 0:
+            this.mensajePersonalizado=HttpErrorResponse.status+': Asegurate de estar conectado a Internet';
+            break;
+          default:
+            this.mensajePersonalizado=HttpErrorResponse.status+": Intentalo nuevamente y/o recarga el navegador";
+            break;
+        }
+        
       }
     )
-  }
+
+  }//find
 
  
   ngOnInit(): void {
